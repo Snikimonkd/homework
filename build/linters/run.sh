8ef6591157464620592fa69350f0debe24ccb905
Snikimonkd <cepminkis132@gmail.com>
2020-10-15 00:07:56 +0300

    linters13

--- build/linters/run.sh
+++ build/linters/run.sh
@@ -4,18 +4,10 @@ set -e
 set -o pipefail
 
 function print_header() {
-    echo -e "\n***** ${1} *****"
+echo -e "\n***** ${1} *****"
 }
 
-print_header "RUN cppcheck"
-if [ "${1}" == "--local" ]; then
-	CPPCHECK="cppcheck"
-else
-	CPPCHECK="./build/linters/cppcheck/cppcheck"
-fi
-${CPPCHECK} project --enable=all --error-exitcode=1 -I include --suppress=missingIncludeSystem # --check-config
-
 print_header "RUN cpplint.py"
-python2.7 ./build/linters/cpplint/cpplint.py --extensions=c project/include/* project/src/*
+python2.7 ./linters/cpplint/cpplint.py —extensions=c —headers=h,hpp —filter=-runtime/references,-legal/copyright,-build/include_subdir,-whitespace/line_length project/include/* project/src/*
 
 print_header "SUCCESS"
