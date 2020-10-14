ac9aa1a9cb326f7da0dd87f22e42fabc1655e6a4
Snikimonkd <cepminkis132@gmail.com>
2020-10-14 22:59:18 +0300

    линтеры2

--- build/linters/run.sh
+++ build/linters/run.sh
@@ -11,11 +11,11 @@ print_header "RUN cppcheck"
 if [ "${1}" == "--local" ]; then
 	CPPCHECK="cppcheck"
 else
-	CPPCHECK="./linters/cppcheck/cppcheck"
+	CPPCHECK="./build/linters/cppcheck/cppcheck"
 fi
-${CPPCHECK} project --enable=all --error-exitcode=1 -I ../include --suppress=missingIncludeSystem # --check-config
+${CPPCHECK} project --enable=all --error-exitcode=1 -I /include --suppress=missingIncludeSystem # --check-config
 
 print_header "RUN cpplint.py"
-python2.7 ./build/linters/cpplint/cpplint.py --extensions=c ../include/* ./src/*
+python2.7 ./build/linters/cpplint/cpplint.py --extensions=c include/* ./src/*
 
 print_header "SUCCESS"
